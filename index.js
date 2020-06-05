6c45d69295e6df626b1e6847d6a4902e0ed1a0d2
Eric Wang <matchstik@MacBook-Pro.local>
2020-06-05 09:22:59 -0700

    working test w/o browser close function

--- index.js
+++ index.js
@@ -10,26 +10,42 @@ chrome.setDefaultService( new chrome.ServiceBuilder( chromedriver.path ).build()
   try {
     await driver.get( 'http://www.amazon.com' );
     await driver.findElement( By.id( 'twotabsearchtextbox' ) ).sendKeys( 'iPhone X', Key.RETURN );
-    await ( await ( await driver.findElement( By.id( 'filters') ) )
-        .findElement( By.id( 'p_n_feature_twenty_browse-bin/17881878011' ) ) )
-        .findElement( By.className( 'a-icon-checkbox' ) )
-        .click();
-    await ( await driver.findElement( By.className( 'a-button-text' ) ) ).click();
+    await ( await driver.findElement( By.xpath( '//*[@id="p_n_feature_twenty_browse-bin/17881878011"]/span/a/div/label/i' ) ) ).click();
+    await driver.wait( until.elementLocated( By.xpath( '//*[@id="a-autoid-0-announce"]' ) ) );
+    await ( await driver.findElement( By.xpath( '//*[@id="a-autoid-0-announce"]' ) ) ).click();
     await ( await driver.findElement( By.linkText( 'Price: High to Low' ) ) ).click();
     await ( await driver.findElements( By.className( 's-include-content-margin' ) ) ).forEach( ( ele ) => {
-        ele.findElement( By.className( 'a-size-medium' ) ).getText()
-          .then( text => console.log( 'Title: ' + text ) )
-          .catch( err => console.log( err ));
-        ele.findElement( By.className( 'a-price-whole' ) ).getText()
-          .then( text => console.log( 'Price: $' + text ) )
-          .catch( err => console.log( err ));
-        ele.findElement( By.tagName( 'a' ) ).getAttribute( 'href' )
-          .then( text => console.log( 'Link to Product: ' + text + '\n' ) )
-          .catch( err => console.log( err ));
-    }).then( driver.quit() );
-  } finally {
-    // await Promise.all( driver.quit() );
+      ele.findElements( By.className( 'a-size-medium' ) ).then( (eleArray ) => {
+        if ( eleArray.length ) {
+          eleArray[0].getText()
+            .then( text => console.log( 'Title: ' + text ) )
+            .catch( err => console.log( err ));
+        }
+      });
+      ele.findElements( By.className( 'a-price-whole' ) ).then( ( eleArray ) => {
+        if ( eleArray.length ) {
+          eleArray[0].getText()
+            .then( text => console.log( 'Price: $' + text ) )
+            .catch( err => console.log( err ));
+        }
+      });
+      ele.findElements( By.tagName( 'a' ) ).then( ( eleArray ) => {
+        if ( eleArray.length ) {
+          eleArray[0].getAttribute( 'href' )
+            .then( text => console.log( 'Link to Product: ' + text + '\n' ) )
+            .catch( err => console.log( err ));
+        }
+      });
+    });
+    // await driver.wait( until.titleIs( 'Amazon.com : iPhone X' ), 10000 );
+    // await driver.quit();
   }
+  catch ( err ) {
+    console.log( err );
+  }
+  // finally {
+  //   await driver.close();
+  // }
 })();
 
 // how to handle error
